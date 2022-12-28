c6e969dd0be87271206025174e4faa2372836053
Josephat Wambugu <josephat.wambugu@mynd.co>
2022-12-28 14:25:22 +0300

    SonarCloud integration

--- src/test/kotlin/com/example/sadapayassignment/SadapayAssignmentApplicationTests.kt
+++ src/test/kotlin/com/example/sadapayassignment/SadapayAssignmentApplicationTests.kt
@@ -1,12 +1,26 @@
 package com.example.sadapayassignment
 
+import org.assertj.core.api.AssertionsForInterfaceTypes.*
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
+import org.junit.jupiter.api.TestInstance
+import org.mockito.Mockito.*
+import org.springframework.boot.SpringApplication
 
-@SpringBootTest
-class SadapayAssignmentApplicationTests {
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class SadapayAssignmentApplicationTests : BaseTest() {
 
 	@Test
 	fun contextLoads() {
+		// Load the application context
+		val context =
+			SpringApplication.run(
+			    SadapayAssignmentApplication::class.java,
+			    "--server.port=8081",
+			    "--spring.main.web-environment=false"
+			)
+
+		// Verify that the context loads successfully
+		assertThat(context).isNotNull
+		assertThat(context.beanDefinitionCount).isGreaterThan(0)
 	}
 }
