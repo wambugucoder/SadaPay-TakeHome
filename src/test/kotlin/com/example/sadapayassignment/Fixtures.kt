a82b81334ee483746178e45eeba6518bb37e271f
Josephat Wambugu <josephat.wambugu@mynd.co>
2022-12-30 13:39:16 +0300

    Custom Transaction Manager

--- src/test/kotlin/com/example/sadapayassignment/Fixtures.kt
+++ src/test/kotlin/com/example/sadapayassignment/Fixtures.kt
@@ -1,11 +1,6 @@
 package com.example.sadapayassignment
 
-import javax.validation.constraints.NotEmpty
-import javax.validation.constraints.NotNull
-import javax.validation.constraints.Size
-
 object Fixtures {
-
     const val POSTGRES_IMAGE = "postgres:11.5"
     const val POSTGRES_DBNAME = "testDb"
     const val POSTGRES_USERNAME = "test"
@@ -13,13 +8,3 @@ object Fixtures {
     const val REPO_PACKAGE = "com.example.sadapayassignment.repository"
     const val MODEL_PACKAGE = "com.example.sadapayassignment.model"
 }
-
-data class TestClass(
-
-    @field:NotNull(message = "field1 must not be null")
-    @field:Size(min = 3, max = 10, message = "field1 must be between 3 and 10 characters")
-    var field1: String? = null,
-
-    @field:NotEmpty(message = "field2 must not be empty")
-    var field2: String? = null
-)
